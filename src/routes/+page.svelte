1b72d8a5ae0797275c8969318834e58e37ccec73
sikutu <sikutu.hiro@gmail.com>
2023-12-02 00:58:35 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    import Child from "./Child.svelte";
+    const names = [1, 2, 3]           //商品名
+    const prices = [100, 200, 300]    //値段
+    let n = names.length
+    let totals = Array(n).fill(0)     //商品単位の合計金額の配列．初期値は全て0
+    let total = 0
+    let nom = Array(n).fill(0)
+    const reset = () => {
+      nom = Array(n).fill(0)
+    }
+    $:{
+      total = 0
+      for (let i=0; i <n; i++){
+        total += totals[i]
+      }
+      total = total
+      console.log(totals)
+    }
+  </script>
+  {#each names as name, i }
+    <Child name = {name} price = {prices[i]} bind:total = {totals[i]} i ={nom[i]}></Child>
+  {/each}
+  
+  
+  合計金額<br>
+  {total}<br>
+  <button on:click={reset}>reset</button><br>
+  
+  
+  <style>
+  
+  </style>
---
+++ src/routes/Child.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let name
+    export let price
+    export let total
+    
+    export let i
+    const cntUP = () => {
+    i = i + 1
+  }
+  const reset = () => {
+    i = 0
+  }
+  $:{
+        total = price * i
+    }
+</script>
+{name}<br>
+{price}<br>
+{total}<br>
+<button on:click={cntUP}>cnt up</button>
+<button on:click={reset}>reset</button><br>
